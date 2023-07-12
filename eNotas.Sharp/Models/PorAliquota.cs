b86a8d43feaa7c0be5ebf927e2332dc0f2578f7a
Guilherme gentile <guilherme.gentile@outlook.com.br>
2023-07-12 11:58:52 -0300

    Adicionado o valor

--- eNotas.Sharp/Models/PorAliquota.cs
+++ eNotas.Sharp/Models/PorAliquota.cs
@@ -7,7 +7,11 @@ namespace eNotas.Sharp.Models
 {
     public partial class PorAliquota
     {
+        [JsonProperty("baseCalculo", NullValueHandling = NullValueHandling.Ignore)]
+        public decimal? BaseCalculo { get; set; }
         [JsonProperty("aliquota", NullValueHandling = NullValueHandling.Ignore)]
         public decimal? Aliquota { get; set; }
+        [JsonProperty("valor", NullValueHandling = NullValueHandling.Ignore)]
+        public decimal? Valor { get; set; }
     }
 }
